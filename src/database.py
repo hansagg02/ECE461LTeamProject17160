bafaa99370fe8a21195ae5ad9159fc629d77f690
zihao hong <hzh853240936@gmail.com>
2024-03-23 18:18:22 -0500

    added test code for database checklogin and createuser def

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from datetime import datetime
 uri = "mongodb+srv://hzh853240936:ETB434x5MsiVYVPT@cluster0.a7zaibg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -10,11 +11,25 @@ try:
 except Exception as e:
     print(e)
 
-def checkLogIn(username,password):
-    return 0
+def checkLogIn(username,password,id):
+    db = client.get_database("UserLoginCredentials")
+    if username in db.list_collection_names:
+        user = db.get_collection(username)
+        myquery = {"userID": id, "password": password}
+        if user.find_one(myquery) is None: 
+            return False
+        return True
+    return False
 
-def createUser(username,pasword):
-    return 0
+def createUser(username,password,id):
+    db = client.get_database("UserLoginCredentials")
+    collection = db[username] 
+    userDocument = {
+        "userID": id,
+        "password": password,
+        "datecreated": datetime.now(),
+    }    
+    collection.insert_one(userDocument)
 
 def getProject(projectID):
     return 0
@@ -24,3 +39,4 @@ def createProject(name,description,projectID):
 
 def operationHW(projectID,qty,index,flag):#check in qty to the index'th project in the projectID, with flag indicating checking in or out.
     return 0
+# Yes sir 
