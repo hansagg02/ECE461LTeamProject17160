009ae440ac2a8312306f543be63d13b817d5832e
zihao hong <hzh853240936@gmail.com>
2024-04-07 15:20:06 -0500

    initial commit for heroku

--- App.py
+++ App.py
@@ -58,7 +58,7 @@ def not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, port=5000)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT',80))
 
 
 
